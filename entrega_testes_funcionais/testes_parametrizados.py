adfb2204fc836c8528aa345e7e613c0b37403f8e
Mayara Cristina da Silva Ferreira <mcristina@img.com.br>
2020-07-14 00:22:34 -0300

    Corrigindo forma de parametrizar e fazer o teste

--- entrega_testes_funcionais/testes_parametrizados.py
+++ entrega_testes_funcionais/testes_parametrizados.py
@@ -10,20 +10,13 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 VALOR_NOMINAL = 500.0
-dias =      [0, 1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 14, 15, 16, 17, 29, 30, 31 ]
-esperado = [-1, 450.0, 450.0, 450.0, 450.0, 475.0, 475.0, 475.0, 475.0, 500.0, 500.0, 500.0, 500.0, 510.5, 511.0, 517.0, 517.5, -1]
-cenarios = []
-resultados = []
-
-### Coletando dados da API
-for index, dia in enumerate(dias):
-    aluguel = Aluguel(VALOR_NOMINAL, dia)
-    resultado = aluguel.calcula_valor()
-    resultados.append(resultado['valor_calculado'])    
-    cenarios.append((dias[index],esperado[index], resultado['valor_calculado']))
+CENARIOS = [(0, -1), (1, 450.0), (2, 450.0), (4, 450.0), (5, 450.0), (6, 475.0), (7, 475.0), (9, 475.0), (10, 475.0), (11, 500.0), (12, 500.0), (14, 500.0), (15, 500.0), (16, 510.5), (17, 511.0), (29, 517.0), (30, 517.5), (31, -1)]
 
 ### Parametrizando os testes
-@pytest.mark.parametrize('dia, esperado,atual', cenarios)
-def test_cenarios(dia, esperado, atual):
+@pytest.mark.parametrize('dia, esperado', CENARIOS)
+def test_cenarios(dia, esperado):
+    aluguel = Aluguel(VALOR_NOMINAL, dia)
+    resultado = aluguel.calcula_valor()
+    atual = resultado['valor_calculado']
     sys.stderr.write("Dia: {} Esperado {} recebido {}".format(dia, esperado, atual))
     assert atual == esperado
